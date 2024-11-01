7d003c3c71a7db490cdf26dbf22122d6e453536f
baa <34665120+baa-mb@users.noreply.github.com>
2024-11-01 11:58:11 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,146 @@
+function setze_bst(nr: number) {
+    for (let x = 0; x <= 4; x++) {
+        for (let y = 0; y <= 7; y++) {
+            // if (Math.floor(x % 8) == (x / 8)) {
+            if (x % 2 == 0) {
+                pos = (4 - x) * 8 + 7 - y
+            } else {
+                // pos = x * 8 + y - 7 * f
+                pos = (4 - x) * 8 + y
+            }
+
+            if (bst[nr][y].charAt(x) == "1") {
+                strip.setPixelColor(pos, neopixel.colors(NeoPixelColors.Orange))
+            } else {
+                strip.setPixelColor(pos, neopixel.colors(NeoPixelColors.Black))
+            }
+        }
+    }
+    // strip.setPixelColor(15, neopixel.colors(NeoPixelColors.Red))
+    strip.show()
+}
+input.onButtonPressed(Button.A, function () {
+    setze_bst(2)
+})
+let pos = 0, akt_bst = 0;
+let strip: neopixel.Strip = null
+let bst: string[][] = []
+let f = 0
+// Hier können Sie weitere Funktionen hinzufügen, die kontinuierlich ausgeführt werden sollen
+let anz_bst = 0
+// Definieren Sie das Buchstabenmuster
+bst = [[
+    "00000",
+    "01110",
+    "10001",
+    "10001",
+    "11111",
+    "10001",
+
+    "10001",
+    "10001"
+], [
+    "00000",
+    "11110",
+    "10001",
+    "10001",
+    "11110",
+    "10001",
+
+    "10001",
+    "11110"
+], [
+    "00000",
+    "11111",
+    "10000",
+    "10000",
+    "10000",
+    "10000",
+    "10000",
+    "11111"
+], [
+    "00000",
+    "11110",
+    "10001",
+    "10001",
+    "10001",
+    "10001",
+    "10001",
+    "11110"
+], [
+    "00000",
+    "11111",
+    "10000",
+    "10000",
+    "11111",
+    "10000",
+    "10000",
+    "11111"
+], [
+    "00000",
+    "11111",
+    "10000",
+    "10000",
+    "11111",
+    "10000",
+    "10000",
+    "10000"
+], [
+    "00000",
+    "01110",
+    "10001",
+    "10001",
+    "10000",
+    "10011",
+    "10001",
+    "01110"
+], [
+    "00000",
+    "10001",
+    "10001",
+    "10001",
+    "11111",
+    "10001",
+    "10001",
+    "10001"
+], [
+    "00000",
+    "11111",
+    "00100",
+    "00100",
+    "00100",
+    "00100",
+    "00100",
+    "11111"
+], [
+    "00000",
+    "00001",
+    "00001",
+    "00001",
+    "00001",
+    "00001",
+    "10001",
+    "01110"
+]
+]
+
+
+
+// Initialisieren Sie den NeoPixel-Strip
+strip = neopixel.create(DigitalPin.P0, 256, NeoPixelMode.RGB)
+strip.setBrightness(6)
+strip.clear()
+strip.show()
+// Initialisierung
+setze_bst(akt_bst)
+basic.forever(function () {
+    // strip.shift(16)
+    strip.rotate(16)
+    strip.show()
+    basic.pause(200)
+    anz_bst++;
+    if ((anz_bst % 4) == 0) {
+        akt_bst = (akt_bst + 1) % 10;
+        setze_bst(akt_bst)
+    }
+})
